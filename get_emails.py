e93c8859bdaef162fdceaab013e213e8e693390b
Margaux <mbavlsik@gmail.com>
2020-06-11 18:32:34 -0500

    Update get_emails.py
    make email extraction into a function

--- get_emails.py
+++ get_emails.py
@@ -8,6 +8,17 @@ import re
 import requests
 import sys
 
+def extract_emails(request_text):
+    # text = request_text.text
+    soup = BeautifulSoup(request_text, 'html.parser')
+    pattern = re.compile("swrot13\('.*'\)")
+    scripts = (soup.find_all("script", text=pattern))
+    for script in scripts:
+        matches = re.search(r'\w*@\w*\.\w*', script.text)
+        #print(matches.group(0))
+        print(codecs.decode(matches.group(0),'rot_13'))
+
+
 # create logger with name
 logger = logging.getLogger('teacher_emails')
 logger.setLevel(logging.DEBUG)
@@ -36,26 +47,11 @@ logger.addHandler(ch)
 directory_url=r'https://go.lindberghschools.ws/Page/14990'
 secondary_url = r'https://go.lindberghschools.ws//cms/UserControls/ModuleView/ModuleViewRendererWrapper.aspx?DomainID=1849&PageID=14990&ModuleInstanceID=21174&PageModuleInstanceID=20912&Tag=&PageNumber=3&RenderLoc=0&FromRenderLoc=0&IsMoreExpandedView=false&EnableQuirksMode=0&Filter=&ScreenWidth=1021&ViewID=00000000-0000-0000-0000-000000000000&_=1591826707309'
 
-r = requests.get(secondary_url)
+# r = requests.get(secondary_url)
+r = requests.get(directory_url)
 logger.info(r.status_code)
-text = r.text
-soup = BeautifulSoup(text, 'html.parser')
-# print(soup.prettify().encode("utf-8"))
 
-pattern = re.compile("swrot13\('.*'\)")
-scripts = (soup.find_all("script", text=pattern))
-
-# email_pattern=re.compile(r'swrot13\(\'?(P<email>\w*)\'\)')
-# test_case = scripts[0].text
-# print(test_case)
-for script in scripts:
-    matches = re.search(r'\w*@\w*\.\w*', script.text)
-    #print(matches.group(0))
-    print(codecs.decode(matches.group(0),'rot_13'))
-# matches = re.search(r'\w*@\w*\.\w*', scripts[0].text)
-# print(matches.group(0))
-
-
+extract_emails(r.text)
 
 
 # <li class="staffemail">
