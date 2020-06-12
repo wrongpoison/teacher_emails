7106753f46283b0e5ffb69e2cd8b98af08d3224c
Margaux <mbavlsik@gmail.com>
2020-06-11 20:15:57 -0500

    clean up email scraper and rename

--- get_emails_lindbergh.py
+++ get_emails_lindbergh.py
@@ -1,5 +1,3 @@
-# from datetime import date, datetime, timedelta
-# import json
 from bs4 import BeautifulSoup
 
 import codecs
@@ -9,13 +7,11 @@ import requests
 import sys
 
 def extract_emails(request_text):
-    # text = request_text.text
     soup = BeautifulSoup(request_text, 'html.parser')
     pattern = re.compile("swrot13\('.*'\)")
     scripts = (soup.find_all("script", text=pattern))
     for script in scripts:
         matches = re.search(r'\w*@\w*\.\w*', script.text)
-        #print(matches.group(0))
         print(codecs.decode(matches.group(0),'rot_13'))
 
 
@@ -23,37 +19,21 @@ def extract_emails(request_text):
 logger = logging.getLogger('teacher_emails')
 logger.setLevel(logging.DEBUG)
 
-# # create file handler logger
-# fh = logging.FileHandler('logger = logging.getLogger('teacher_emails')
-# .log')
-# fh.setLevel(logging.DEBUG)
-
 # create console handler logger 
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 
 # create formatter and add to handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# fh.setFormatter(formatter)
 ch.setFormatter(formatter)
-
-# add handlers to logger
-# logger.addHandler(fh)
 logger.addHandler(ch)
-
-#DIRECTORY_URL =sys.argv[1]
-#logger.info(DIRECTORY_URL)
 
 directory_url=r'https://go.lindberghschools.ws/Page/14990'
 secondary_url = r'https://go.lindberghschools.ws//cms/UserControls/ModuleView/ModuleViewRendererWrapper.aspx?DomainID=1849&PageID=14990&ModuleInstanceID=21174&PageModuleInstanceID=20912&Tag=&PageNumber=3&RenderLoc=0&FromRenderLoc=0&IsMoreExpandedView=false&EnableQuirksMode=0&Filter=&ScreenWidth=1021&ViewID=00000000-0000-0000-0000-000000000000&_=1591826707309'
 
-# r = requests.get(secondary_url)
-r = requests.get(directory_url)
-logger.info(r.status_code)
-
-extract_emails(r.text)
-
-
-# <li class="staffemail">
-# href="mailto:xxx"
-
+for x in range(50):
+    url=r'https://go.lindberghschools.ws//cms/UserControls/ModuleView/ModuleViewRendererWrapper.aspx?DomainID=1849&PageID=14990&ModuleInstanceID=21174&PageModuleInstanceID=20912&Tag=&PageNumber=' + str(x) + '&RenderLoc=0&FromRenderLoc=0&IsMoreExpandedView=false&EnableQuirksMode=0&Filter=&ScreenWidth=1021&ViewID=00000000-0000-0000-0000-000000000000&_=1591826707309'
+    logger.info(url)
+    r = requests.get(url)
+    logger.info(r.status_code)
+    extract_emails(r.text)
